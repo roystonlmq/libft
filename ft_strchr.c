800e560028485b62a9e37c157216382376a2a140
Royston Lee <roylee@c1r6s1.42singapore.sg>
2023-09-08 23:49:38 +0800

    submit strchr

---
+++ ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 21:33:51 by roylee            #+#    #+#             */
+/*   Updated: 2023/09/08 23:49:26 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == 0)
+		return ((char *)s);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	const char	test[] = "hello";
+	int		c = 0;
+
+	printf("%s", ft_strchr(test, c));
+}
+*/
