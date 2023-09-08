bd66ab913a786e4f0099429c2b7145f02271522b
Royston Lee <roylee@c1r6s1.42singapore.sg>
2023-09-08 20:58:12 +0800

    add memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 21:35:57 by roylee            #+#    #+#             */
+/*   Updated: 2023/09/06 21:43:01 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dest_cpy;
+	unsigned char	*src_cpy;
+
+	dest_cpy = (unsigned char *)dest;
+	src_cpy = (unsigned char *)src;
+	while (n > 0)
+	{
+		*(dest_cpy++) = *(src_cpy++);
+		n--;
+	}
+	return (dest);
+}
