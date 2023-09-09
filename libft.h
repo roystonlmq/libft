9458d521261fee9f796cdd11232c25120301112d
Royston Lee <roylee@c1r8s1.42singapore.sg>
2023-09-09 13:54:41 +0800

    update libft.h with fns

---
+++ libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 12:32:14 by roylee            #+#    #+#             */
+/*   Updated: 2023/09/09 13:20:18 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h> // Unused header
+# include <stdio.h> // Unused header
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char	*nptr);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+#endif
