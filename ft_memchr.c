64ec5aa6a79890121fbf3ecf20e81bbf22e9581e
Royston Lee <roylee@c1r6s1.42singapore.sg>
2023-09-11 20:26:21 +0800

    fix bug

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 11:48:25 by roylee            #+#    #+#             */
-/*   Updated: 2023/09/09 12:08:12 by roylee           ###   ########.fr       */
+/*   Updated: 2023/09/10 21:50:24 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
-	unsigned char	d;
+	size_t			i;
 
 	ptr = (unsigned char *)s;
-	d = (unsigned char)c;
-	if (n == 0)
-		return (NULL);
-	while (*ptr && n > 0)
+	i = 0;
+	while (n > i)
 	{
-		if (*ptr == d)
-			return (ptr);
-		ptr++;
-		n--;
+		if (ptr[i] == (unsigned char)c)
+			return (&ptr[i]);
+		i++;
 	}
 	return (NULL);
 }
@@ -35,29 +32,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 #include <string.h>
 #include <stdio.h>
 int main() {
-    const char *str = "This is a sample string";
-    int ch = 's';
-    size_t len = 2;
-
-    // Test 1: Find the first occurrence of 's' in str
-    char *result = (char *)memchr(str, ch, len);
-    char *result2 = (char *)ft_memchr(str, ch, len);
-	if (result2 != NULL) {
-        printf("Found '%c' at position %ld\n", ch, result - str);
-    } else {
-        printf("'%c' not found in the string\n", ch);
-    }
-
-    // Test 2: Try to find a character that doesn't exist
-    ch = 'z';
-    result = (char *)memchr(str, ch, len);
-    result2 = (char *)ft_memchr(str, ch, len);
-	if (result2 != NULL) {
-        printf("Found '%c' at position %ld\n", ch, result - str);
-    } else {
-        printf("'%c' not found in the string\n", ch);
-    }
-
-    return 0;	
+	char s[] = {0, 1, 2, 3, 4, 5};
+	char *ptr = ft_memchr(s, 2, 3);
+	printf("%s", ptr);
 }
 */
