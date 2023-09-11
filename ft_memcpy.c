f73f10f1b7e0ca2aef360643f12e37c0663e68c6
Royston Lee <roylee@c1r6s1.42singapore.sg>
2023-09-11 20:26:42 +0800

    submit memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 21:35:57 by roylee            #+#    #+#             */
-/*   Updated: 2023/09/09 14:17:34 by roylee           ###   ########.fr       */
+/*   Updated: 2023/09/11 20:24:09 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,24 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*dest_cpy;
 	unsigned char	*src_cpy;
+	size_t			i;
 
+	if (!dest && !src)
+		return (0);
 	dest_cpy = (unsigned char *)dest;
 	src_cpy = (unsigned char *)src;
-	while (n > 0)
+	i = 0;
+	while (n > 0 && i < n)
 	{
-		*(dest_cpy++) = *(src_cpy++);
-		n--;
+		dest_cpy[i] = src_cpy[i];
+		i++;
 	}
 	return (dest);
 }
+
+/*
+int	main(void)
+{
+	ft_memcpy(((void *)0), ((void *)0), 3);
+}
+*/
