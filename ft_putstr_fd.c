f9edcbcac10ef2e8e9ebdd1a743e2b9923f981b9
royston <roylee@student.42singapore.sg>
2023-09-17 16:59:03 +0800

    revert return type to void

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,25 +6,19 @@
 /*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 20:41:31 by roylee            #+#    #+#             */
-/*   Updated: 2023/09/16 18:42:58 by roylee           ###   ########.fr       */
+/*   Updated: 2023/09/17 16:57:45 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	int	len;
-
-	len = 0;
 	if (!s)
-		return (0);
+		return ;
 	while (*s)
-	{
 		ft_putchar_fd(*(s++), fd);
-		len++;
-	}
-	return (len);
+	return ;
 }
 /*
 int main(void)
